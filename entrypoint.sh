10b347b4d252422608af550c54ea979c85bb2086
Chad Gilbert <chad.gilbert@cqlcorp.com>
2017-08-28 22:32:18 -0400

    Fix swarm init arg passing
    Specifically, whitespace-separated labels were have problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ FSROOT="${FSROOT:-/tmp/jenkins}"
 NODENAMEPREFIX="${NODENAMEPREFIX:-docker-}"
 
 mkdir -p $FSROOT
-java -jar swarm-client.jar -labels="$LABELS" -executors="$EXECUTORS" -fsroot=/tmp/jenkins -name=${NODENAMEPREFIX}$(hostname) $(cat /run/secrets/jenkins | sed -e 's/\r$//')
+java -jar swarm-client.jar -labels "$LABELS" -executors "$EXECUTORS" -fsroot /tmp/jenkins -name ${NODENAMEPREFIX}$(hostname) $(cat /run/secrets/jenkins | sed -e 's/\r$//')
